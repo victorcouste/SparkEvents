6458efd0297374e4e33fb611a616e0193c2104ee
victorcouste <victor.coustenoble@gamil.com>
2015-04-11 17:52:46 +0200

    new v0 ok

--- build.sbt
+++ build.sbt
@@ -9,11 +9,11 @@ organization := "datastax.com"
 
 resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
 
-libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.1.0" % "provided"
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.1.0"// % "provided"
 
-libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.1.0" % "provided"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.1.0"// % "provided"
 
-libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "1.1.1" % "provided" //withSources() withJavadoc()
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "1.1.1" //% "provided" //withSources() withJavadoc()
 
 //We do this so that Spark Dependencies will not be bundled with our fat jar but will still be included on the classpath
 //When we do a sbt/run
--- src/main/scala/EventsAggregations.scala
+++ src/main/scala/EventsAggregations.scala
@@ -1,8 +1,10 @@
 
+import org.apache.spark.SparkContext._
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql.CassandraConnector
 import org.apache.spark.sql.cassandra.CassandraSQLContext
 import org.apache.spark.{SparkContext, SparkConf}
+import org.joda.time.DateTime
 
 object EventsAggregations  {
 
@@ -16,15 +18,34 @@ object EventsAggregations  {
       .setMaster("local")
       .setAppName("spark-events")
 
-
-    //val sc = new SparkContext("local[2]", "test", sparkConf)
     val sc = new SparkContext(sparkConf)
-    val allevents = sc.cassandraTable("ks_events", "events")
+    val sqlc = new CassandraSQLContext(sc)
 
-    println("count: " + allevents.count)
 
-    //allstarful.foreach(println(_))
-    println("first: " + allevents.first)
+    //  --------- GROUP BY WITH SPARK ------------------------------------
+
+    sc.cassandraTable("ks_events", "events").select("evt_id","evt_veh","evt_gts")
+      .map( r => (r.get[String]("evt_id"),1))
+      .reduceByKey(_ + _)
+      //.foreach(println)
+      .saveToCassandra("ks_events", "events_byevtid", SomeColumns("evt_id","nb_evts"))
+
+    //  --------- GROUP BY WITH SQL ------------------------------------
+
+    //sqlc.sql("select evt_id, count(*) from ks_events.events group by evt_id")
+    //.toArray.foreach(println)
+    //case class CountEvtID(evtid: String, count: Int)
+    // .map( p => CountEvtID(p(0),p(1)))
+    // OR  val .map( p => (p(0),p(1)))
+    //.saveToCassandra("ks_events", "events_byevtid", SomeColumns("evt_id","nb_evts"))
+
+    sqlc.sql("select evt_veh, count(*) from ks_events.events group by evt_veh")
+      .map( p => (p(0),p(1)))
+      .saveToCassandra("ks_events", "events_byevtveh", SomeColumns("evt_veh","nb_evts"))
+
+
+    //println("count: " + eventsbyevtid.count)
+    //println("first: " + eventsbyevtveh.first)
 
     sc.stop()
 
